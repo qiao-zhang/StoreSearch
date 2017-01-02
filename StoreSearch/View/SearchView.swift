fdef48fe42936a5ef77d408fca36e21745624ce8
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-01-01 21:38:57 -0500

    rename to SearchView

--- StoreSearch/View/SearchView.swift
+++ StoreSearch/View/SearchView.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchView.swift
 //  StoreSearch
 //
 //  Created by Qiao Zhang on 1/1/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchView: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
 
 }
 
